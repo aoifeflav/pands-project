c62d6825fc67154f33e42effdee96001f05dcbd7
Aoife Flavin <G00439331@atu.ie>
2024-05-05 19:40:32 +0100

    bar chart added

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ Get these tasks done by 12/05 and from there add extras
 * Create a histrgram of sepal width
 * Create a histrgram of petal length
 * Create a histrgram of petal width
-* Create a bar chart of flower type
+* ~~Create a bar chart of flower type~~
 * Create a scatter plot of sepal length x width
 * Create a scater plot of petal length x width
 * Create a scater plot of petal length x sepal width
--- analysis.py
+++ analysis.py
@@ -15,6 +15,30 @@ import matplotlib.pyplot as plt
 # Load the dataset
 df = pd.read_csv("iris.csv")
 
-
+'''
 #take a look
 print(df.head())
+'''
+
+#count how many of each species there are
+species_amount = df['species'].value_counts()
+
+#put it in a bar chart
+plt.figure(figsize=(10, 6)) # make it bigger
+species_amount.plot(kind = 'bar', color = ['#F2C6DE', '#DBCDF0', '#C6DEF1'], edgecolor='black')
+plt.ylabel("No. of Flowers Spotted") 
+plt.xlabel("Species")
+plt.xticks(rotation=0) #straighten bottom labels
+plt.title("Amount of each Species Spotted")
+plt.gca().spines['top'].set_visible(False) #remove line a top
+plt.gca().spines['right'].set_visible(False) # reove line on right
+
+plt.show()
+
+
+
+
+
+'''
+plen = df['petal_length']
+'''
