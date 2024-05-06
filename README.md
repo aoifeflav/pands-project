b08734477c7d29e4fc4af8769b52f81b231380d9
Aoife Flavin <G00439331@atu.ie>
2024-05-06 16:42:30 +0100

    scatter plots added

--- README.md
+++ README.md
@@ -8,18 +8,20 @@ Get these tasks done by 12/05 and from there add extras
 * ~~Download dataset & add to repository (done in Ian's lectures)~~
 * Write a summary of each variable
 * have analysis.py output a summary of each variable to a single text file (extracting the result of pandas . describe() of each variable to a txt file.)
-* Create a histrgram of sepal length
-* Create a histrgram of sepal width
-* Create a histrgram of petal length
-* Create a histrgram of petal width
+* ~~Create a histrgram of sepal length~~
+* ~~Create a histrgram of sepal width~~
+* ~~Create a histrgram of petal length~~
+* ~~Create a histrgram of petal width~~
 * ~~Create a bar chart of flower type~~
-* Create a scatter plot of sepal length x width
-* Create a scater plot of petal length x width
-* Create a scater plot of petal length x sepal width
-* Create a scater plot of petal length x sepal length
-* Create a scatter plot of sepal length x petal width
+* ~~Create a scatter plot of sepal length x width~~
+* ~~Create a scater plot of petal length x width~~
+* ~~Create a scater plot of petal length x sepal width~~
+* ~~Create a scater plot of petal length x sepal length~~
+* ~~Create a scatter plot of petal width x sepal width~~
+* ~~Create a scatter plot of petal width x sepal length~~
 
 
 ## Sources
 https://en.wikipedia.org/wiki/Iris_flower_data_set#:~:text=The%20Iris%20flower%20data%20set,example%20of%20linear%20discriminant%20analysis.
 https://github.com/mwaskom/seaborn-data/blob/master/iris.csv
+Ian's lecture notes as a source
--- analysis.py
+++ analysis.py
@@ -35,7 +35,7 @@ plt.xticks(rotation=0) #straighten bottom labels
 plt.title("Amount of each Species Spotted")
 plt.gca().spines['top'].set_visible(False) #remove line a top
 plt.gca().spines['right'].set_visible(False) # reove line on right
-'''
+
 
 
 #make a histogram for sepal length
@@ -87,7 +87,114 @@ plt.gca().spines['top'].set_visible(False) #remove line on top
 plt.gca().spines['right'].set_visible(False) #remove line on right
 
 plt.show()
+'''
 
-'''
+
+# get the petal + sepal length and width from the data and name them as variables
 plen = df['petal_length']
-'''
+pwidth = df['petal_width']
+slen = df['sepal_length']
+swidth = df['sepal_width']
+
+#1 Plot petal length vs petal width
+
+#Use the stateful approach
+fig, ax = plt.subplots(figsize=(10,6))
+
+#plot it and make it pretty
+ax.scatter(plen, pwidth, color='orange', alpha=0.6, s=80, edgecolor='black')
+ax.set_xlabel('Petal Length', fontsize=12)
+ax.set_ylabel('Petal Width', fontsize=12)
+ax.set_title('Petal Length vs Petal Width', fontsize=14)
+ax.grid(True, linestyle='--', alpha=0.7) #add graph lines
+ax.tick_params(axis='x',labelsize=10)
+ax.tick_params(axis='y',labelsize=10)
+fig.tight_layout()
+
+
+
+
+#2 Plot petal length vs sepal length
+
+#Use the stateful approach
+fig, ax = plt.subplots(figsize=(10,6))
+
+#plot it and make it pretty
+ax.scatter(plen, slen, color='orange', alpha=0.6, s=80, edgecolor='black')
+ax.set_xlabel('Petal Length', fontsize=12)
+ax.set_ylabel('Sepal Length', fontsize=12)
+ax.set_title('Petal Length vs Sepal Length', fontsize=14)
+ax.grid(True, linestyle='--', alpha=0.7) #add graph lines
+ax.tick_params(axis='x',labelsize=10)
+ax.tick_params(axis='y',labelsize=10)
+fig.tight_layout()
+
+
+
+#3 Plot petal length vs sepal width
+
+#Use the stateful approach
+fig, ax = plt.subplots(figsize=(10,6))
+
+#plot it and make it pretty
+ax.scatter(plen, swidth, color='orange', alpha=0.6, s=80, edgecolor='black')
+ax.set_xlabel('Petal Length', fontsize=12)
+ax.set_ylabel('Sepal Width', fontsize=12)
+ax.set_title('Petal Length vs Sepal Width', fontsize=14)
+ax.grid(True, linestyle='--', alpha=0.7) #add graph lines
+ax.tick_params(axis='x',labelsize=10)
+ax.tick_params(axis='y',labelsize=10)
+fig.tight_layout()
+
+
+
+#4 Plot petal width vs sepal length
+
+#Use the stateful approach
+fig, ax = plt.subplots(figsize=(10,6))
+
+#plot it and make it pretty
+ax.scatter(pwidth, slen, color='orange', alpha=0.6, s=80, edgecolor='black')
+ax.set_xlabel('Petal Width', fontsize=12)
+ax.set_ylabel('Sepal Length', fontsize=12)
+ax.set_title('Petal Width vs Sepal Length', fontsize=14)
+ax.grid(True, linestyle='--', alpha=0.7) #add graph lines
+ax.tick_params(axis='x',labelsize=10)
+ax.tick_params(axis='y',labelsize=10)
+fig.tight_layout()
+
+
+
+#5 Plot petal width vs sepal width
+
+#Use the stateful approach
+fig, ax = plt.subplots(figsize=(10,6))
+
+#plot it and make it pretty
+ax.scatter(pwidth, swidth, color='orange', alpha=0.6, s=80, edgecolor='black')
+ax.set_xlabel('Petal Width', fontsize=12)
+ax.set_ylabel('Sepal Width', fontsize=12)
+ax.set_title('Petal Width vs Sepal Width', fontsize=14)
+ax.grid(True, linestyle='--', alpha=0.7) #add graph lines
+ax.tick_params(axis='x',labelsize=10)
+ax.tick_params(axis='y',labelsize=10)
+fig.tight_layout()
+
+
+
+#6 Plot sepal length vs sepal width
+
+#Use the stateful approach
+fig, ax = plt.subplots(figsize=(10,6))
+
+#plot it and make it pretty
+ax.scatter(slen, swidth, color='orange', alpha=0.6, s=80, edgecolor='black')
+ax.set_xlabel('Sepal Length', fontsize=12)
+ax.set_ylabel('Sepal Width', fontsize=12)
+ax.set_title('Sepal Length vs Sepal Width', fontsize=14)
+ax.grid(True, linestyle='--', alpha=0.7) #add graph lines
+ax.tick_params(axis='x',labelsize=10)
+ax.tick_params(axis='y',labelsize=10)
+fig.tight_layout()
+
+plt.show()
